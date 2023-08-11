718104c3b2dbc0004d1521541e4d1fef45efecdd
NGustavo011 <gsn181200@gmail.com>
2023-08-11 17:39:33 -0300

    docs: add bo2-wildcard path in swagger

--- src/main/docs/swagger/schemas/swagger-schemas.ts
+++ src/main/docs/swagger/schemas/swagger-schemas.ts
@@ -1,6 +1,7 @@
 import { bo2MapSchema } from './bo2/bo2-map-schema'
 import { bo2PerkSchema } from './bo2/bo2-perk-schema'
 import { bo2WeaponSchema } from './bo2/bo2-weapon-schema'
+import { bo2WildcardSchema } from './bo2/bo2-wildcard-schema'
 import { errorSchema } from './shared/error-schema'
 
 
@@ -8,6 +9,6 @@ export const swaggerSchemas = {
   bo2Map: bo2MapSchema,
   bo2Perk: bo2PerkSchema,
   bo2Weapon: bo2WeaponSchema,
-  
+  bo2Wildcard: bo2WildcardSchema,
   error: errorSchema,
 }
