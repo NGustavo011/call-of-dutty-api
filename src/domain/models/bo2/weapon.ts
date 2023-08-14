22e4012df467a16219fed72a2501b5e4b1d83c42
NGustavo011 <gsn181200@gmail.com>
2023-08-14 16:30:12 -0300

    feat: add weapon model interface

---
+++ src/domain/models/bo2/weapon.ts
@@ -0,0 +1,19 @@
+export interface Bo2WeaponModel {
+    id: string
+    name: string,
+    weaponClass: string,
+    damage: string,
+    rateOfFire: string,
+    magazineSize: string,
+    startingAmmunition: string,
+    maximumAmmunition: string,
+    reloadTime: string,
+    damageDropStart: string,
+    damageDropEnd: string,
+    recoil: string,
+    penetration: string,
+    unlocked: string,
+    icon: string,
+    hudIcon: string
+}
+  
