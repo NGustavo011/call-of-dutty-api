e81bb21faeb05fceb690113c646008b09a6e4963
NGustavo011 <gsn181200@gmail.com>
2023-08-14 15:28:52 -0300

    docs: add bo2-attachment path in swagger

--- src/main/docs/swagger/paths/swagger-paths.ts
+++ src/main/docs/swagger/paths/swagger-paths.ts
@@ -1,3 +1,4 @@
+import { bo2AttachmentPath } from './bo2/attachments/bo2-attachment-path'
 import { bo2MapPath } from './bo2/maps/bo2-map-path'
 import { bo2PerkPath } from './bo2/perks/bo2-perk-path'
 import { bo2ScorestreakPath } from './bo2/scorestreaks/bo2-scorestreak-path'
@@ -5,6 +6,7 @@ import { bo2WeaponPath } from './bo2/weapons/bo2-weapon-path'
 import { bo2WildcardPath } from './bo2/wildcards/bo2-wildcard-path'
 
 export const swaggerPaths = {
+  '/bo2/attachment': bo2AttachmentPath,
   '/bo2/map': bo2MapPath,
   '/bo2/perk': bo2PerkPath,
   '/bo2/scorestreak': bo2ScorestreakPath,
--- src/main/docs/swagger/schemas/swagger-schemas.ts
+++ src/main/docs/swagger/schemas/swagger-schemas.ts
@@ -1,3 +1,4 @@
+import { bo2AttachmentSchema } from './bo2/bo2-attachment-schema'
 import { bo2MapSchema } from './bo2/bo2-map-schema'
 import { bo2PerkSchema } from './bo2/bo2-perk-schema'
 import { bo2ScorestreakSchema } from './bo2/bo2-scorestreak-schema'
@@ -7,6 +8,7 @@ import { errorSchema } from './shared/error-schema'
 
 
 export const swaggerSchemas = {
+  bo2Attachment: bo2AttachmentSchema,
   bo2Map: bo2MapSchema,
   bo2Perk: bo2PerkSchema,
   bo2Scorestreak: bo2ScorestreakSchema,
